b7eee177df722121a31a38948a9ed7d0625a0d74
Brian Jennings <brian.j.jennings@gmail.com>
2016-09-26 16:44:26 -0700

    Add specs around the audio session category

--- BestPracticesTests/AppDelegateSpec.swift
+++ BestPracticesTests/AppDelegateSpec.swift
@@ -1,30 +1,32 @@
 import Quick
 import Nimble
 import Fleet
+import AVFoundation
+import AVKit
 @testable import BestPractices
 
 class AppDelegateSpec: QuickSpec {
     override func spec() {
-        
+
         var subject: AppDelegate!
         var application: MockApplication!
         var applicationProvider: MockApplicationProvider!
-        
+
         beforeEach {
             subject = AppDelegate()
-            
+
             applicationProvider = MockApplicationProvider()
             subject.applicationProvider = applicationProvider
         }
-        
+
         describe(".application:didFinishLaunchingWithOptions:") {
             beforeEach {
                 application = MockApplication()
                 applicationProvider.returnValueForSharedApplication = application
-                
+
                 subject.application(UIApplication.sharedApplication(), didFinishLaunchingWithOptions: nil)
             }
-            
+
             it("loads the view controller into the window") {
                 expect(subject.window).toNot(beNil())
                 expect(subject.window!.keyWindow).to(beTruthy())
@@ -33,15 +35,20 @@ class AppDelegateSpec: QuickSpec {
                 expect(subject.window!.rootViewController!).to(beAKindOf(UINavigationController))
                 let navController = subject.window!.rootViewController! as! UINavigationController
                 expect(navController.topViewController).toNot(beNil())
-                expect(navController.topViewController!).to(beAKindOf(HomeViewController))                
+                expect(navController.topViewController!).to(beAKindOf(HomeViewController))
             }
-            
+
             it("calls the application provider") {
                 expect(applicationProvider.calledSharedApplication).to(beTruthy())
             }
-            
+
             it("starts listening for remote control events") {
                 expect(application.calledBeginReceivingRemoteControlEvents).to(beTruthy())
+            }
+
+            it("sets the audio session category") {
+                expect(AVAudioSession.sharedInstance()).toNot(beNil())
+                expect(AVAudioSession.sharedInstance().category).to(equal(AVAudioSessionCategoryPlayback))
             }
         }
     }
