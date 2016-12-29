7e86ad3836d33fc75683394c1d052b1c2d34d3ff
Brian Jennings <pair+bjennings@pivotal.io>
2016-12-29 11:48:36 -0800

    Put in icon for refreshing in list view

--- BestPractices/UI/PullToRefreshTableViewCell.swift
+++ BestPractices/UI/PullToRefreshTableViewCell.swift
@@ -1,7 +1,16 @@
 import UIKit
+import Font_Awesome_Swift
 
 class PullToRefreshTableViewCell: UITableViewCell {
 
-    static let cellIdentifier = "PullToRefreshTableViewCell"        
+    @IBOutlet weak var infoLabel: UILabel!
+    
+    static let cellIdentifier = "PullToRefreshTableViewCell"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        self.infoLabel.setFAText(prefixText: "Pull to Refresh  ", icon: FAType.FARefresh, postfixText: "", size: 14.0, iconSize: 14.0)
+    }
 
 }
